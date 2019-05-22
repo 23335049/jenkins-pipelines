9dacc46c7c29ab553163b1669672f48d994b4ec2
王雄飞 <23335049@qq.com>
2019-05-22 15:55:35 +0800

    add get email from git

--- src/testfun.groovy
+++ src/testfun.groovy
@@ -4,15 +4,11 @@ pipeline {
         stage('checkout') {
             steps {
                 git branch:'deploy', url: 'https://github.com/23335049/cache-timeout.git'
-                script {
-                    def lastCommitEmail = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%ae'")
-                    echo lastCommitEmail
-                }
             }
         }
         stage('test_mail'){
             steps {
-                emailext to: "$params.lastCommitEmail",
+                emailext to: "${sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%ae'")}",
                          subject: 'test_mail',
                          body: 'a test mail.'
             }
