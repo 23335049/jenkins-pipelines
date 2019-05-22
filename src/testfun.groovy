0f9f969f0865b6618da712b785b3ab64582d297a
王雄飞 <23335049@qq.com>
2019-05-22 14:18:26 +0800

    add steps

--- src/testfun.groovy
+++ src/testfun.groovy
@@ -2,7 +2,9 @@ pipeline {
     agent any
     stages {
         stage('test_mail'){
-            mail(to: '23335049@qq.com', subject: 'test_mail', body: 'a test mail.')
+            steps {
+                mail(to: '23335049@qq.com', subject: 'test_mail', body: 'a test mail.')
+            }
         }
     }
 }
