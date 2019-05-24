00168efb5d691c6e1a835f8e43423cafed859474
王雄飞 <23335049@qq.com>
2019-05-24 14:45:01 +0800

    add get env from git

--- src/testfun.groovy
+++ src/testfun.groovy
@@ -1,21 +1,30 @@
 pipeline {
     agent any
+    par
     stages {
         stage('checkout') {
             steps {
                 git branch:'deploy', url: 'https://github.com/23335049/cache-timeout.git'
             }
         }
-        stage('test_mail'){
+//        stage('test_mail'){
+//            environment {
+//                LAST_COMMIT_EMAIL = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%ae'")
+//            }
+//            steps {
+//                emailext to: LAST_COMMIT_EMAIL,
+//                         subject: 'test_mail',
+//                         body: 'a test mail.'
+//            }
+//        }
+        stage("test_env") {
             environment {
                 LAST_COMMIT_EMAIL = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%ae'")
             }
             steps {
-                echo LAST_COMMIT_EMAIL
-                echo env.LAST_COMMIT_EMAIL
-                emailext to: LAST_COMMIT_EMAIL,
-                         subject: 'test_mail',
-                         body: 'a test mail.'
+                script {
+                    env.properties.each {echo it}
+                }
             }
         }
     }
